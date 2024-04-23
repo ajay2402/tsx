37a30a4d18bbe7b3c7fb3775b66b3780f2c2e754
Ajay.M <ajaymanivannan02@gmail.com>
2024-04-23 15:42:52 +0530

    add files

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import  Search from './Component/Search';
+
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+      <Search/>
+    </div>
+  );
+}
+
+export default App;
